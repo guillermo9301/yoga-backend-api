5843802f2dc904c74f07703093ed709763420f92
guillermo9301 <guille.anticona@gmail.com>
2024-07-10 11:40:02 -0500

    add CommandLineRunner para crear defaultAdmin

--- src/main/java/pe/oly/yoga_backend_api/Config/ApplicationConfig.java
+++ src/main/java/pe/oly/yoga_backend_api/Config/ApplicationConfig.java
@@ -1,5 +1,8 @@
 package pe.oly.yoga_backend_api.Config;
 
+import java.time.LocalDate;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,7 +15,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import lombok.RequiredArgsConstructor;
+import pe.oly.yoga_backend_api.User.Rol;
 import pe.oly.yoga_backend_api.User.UserRepository;
+import pe.oly.yoga_backend_api.User.Usuario;
 
 @Configuration
 @RequiredArgsConstructor
@@ -42,6 +47,36 @@ public class ApplicationConfig {
     public UserDetailsService userDetailsService() {
         return username -> userRepository.findByCorreo(username)
                 .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado!!"));
+    }
+
+    @Bean
+    public CommandLineRunner init() {
+        return args -> {
+            String adminCorreo = "admin@correo.com";
+            if (!userRepository.findByCorreo(adminCorreo).isPresent()) {
+                Usuario defaultAdmin = new Usuario();
+                defaultAdmin.setCorreo(adminCorreo);
+                defaultAdmin.setNombre("admin");
+                defaultAdmin.setApellido_paterno("adminAp");
+                defaultAdmin.setApellido_materno("adminAm");
+                defaultAdmin.setFec_nacimiento(LocalDate.of(2000, 6, 20));
+                defaultAdmin.setId_tipo_documento(1);
+                defaultAdmin.setNro_documento("74584231");
+                defaultAdmin.setPassword(passwordEncoder().encode("admin"));
+                defaultAdmin.setRol(Rol.ADMIN);
+                userRepository.save(defaultAdmin);
+                System.out.println("Se creo el usuario admin por default");
+                System.out.println("credenciales:");
+                System.out.println("correo: admin@correo.com");
+                System.out.println("password: admin");
+            } else {
+                System.out.println("El usuario admin por defecto ya existe:");
+                System.out.println("credenciales: ");
+                System.out.println("correo: admin@correo.com");
+                System.out.println("password: admin");
+            }
+            ;
+        };
     }
 
 }
