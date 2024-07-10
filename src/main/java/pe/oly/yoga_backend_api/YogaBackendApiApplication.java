c8fb76428942eec7fb7d0c4a20ff525bf829e8f0
guillermo9301 <guille.anticona@gmail.com>
2024-07-10 12:47:30 -0500

    config variables de entorno y dotenv

--- src/main/java/pe/oly/yoga_backend_api/YogaBackendApiApplication.java
+++ src/main/java/pe/oly/yoga_backend_api/YogaBackendApiApplication.java
@@ -3,10 +3,17 @@ package pe.oly.yoga_backend_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class YogaBackendApiApplication {
 
 	public static void main(String[] args) {
+		// Cargar variables de entorno desde el archivo .env
+		Dotenv dotenv = Dotenv.load();
+
+		// Establecer las variables de entorno
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(YogaBackendApiApplication.class, args);
 	}
 
